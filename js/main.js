de872c092676e97378c7c0718ad88257c2190602
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2020-10-18 23:06:23 -0400

    Updated - Sizing issues fixed - Video now displays when Milestone2 is first clicked - Change content to appear in Modals except video and pdf - Added some borders to the nav-pills - Added background color for nav-pills - Removed excess JavaScript

--- js/main.js
+++ js/main.js
@@ -19,20 +19,25 @@
 })(jQuery);
 
 
-//Click to change content
+//Function that changes milestone content
 function toContent(changeTo){
+	//Hide all milestone content on page load
 	$(".components").on("click", "*", function(){
 		$('#wrapper').find('.content:visible').each(function(){
-	  	///$(this).fadeOut("fast");
 			$(this).hide();
 		});
 
+		//If switching back to project description
 		if (changeTo == -1){
 			$("#project_description").show();
 		}
+		//Else, switch to appropriate milestone
 		else {
 			$("#milestone" + changeTo + "-content").show();
-			$("#milestone" + changeTo + "-content").height("100%");
+
+			if (changeTo == 2){
+				$("#pill-video").click();
+			}
 
 		}
 	});
